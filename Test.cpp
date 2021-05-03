3fe6a83640dc71e2aa99690e7c358b833f67172b
kfir <kggold4@gmail.com>
2021-05-03 10:58:09 +0300

    create test.cpp

---
+++ Test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "doctest.h"
+#include "Board.hpp"
+#include "City.hpp"
+#include "Color.hpp"
+#include "Dispatcher.hpp"
+#include "FieldDoctor.hpp"
+#include "GeneSplicer.hpp"
+#include "Player.hpp"
+#include "Researcher.hpp"
+#include "Scientist.hpp"
+#include "Virologist.hpp"
+
+using namespace std;
+using namespace pandemic;
+
+TEST_CASE("base case") {
+    CHECK();
+}
