54104f33d64c3e34c671182ee79d4f9745e121c0
kfir <kggold4@gmail.com>
2021-05-03 10:54:04 +0300

    start writing signaturs of methods in player class and create city enum

--- Player.hpp
+++ Player.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include "Color.hpp"
+#include "City.hpp"
+
+namespace pandemic {
+    class Player {
+        private:
+            pandemic::City current_city;
+            unsigned int number_of_cards;
+            char number_of_research_station;
+        public:
+            Player();
+            void drive(City city);
+            void fly_direct(City city);
+            void fly_charter(City city);
+            void fly_shuttle(City city);
+            void bulid();
+            void discover_curve();
+            void treat();
+            void role();
+            void take_card();
+    };
+}
