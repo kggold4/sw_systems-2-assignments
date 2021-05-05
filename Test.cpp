0998ecbf6e4d88e53bf30a4cd53afe6784a570c8
kfir <kggold4@gmail.com>
2021-05-05 13:02:48 +0300

    add is_clean method in board testing case

--- Test.cpp
+++ Test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <random>
 
 #include "doctest.h"
 #include "Board.hpp"
@@ -16,29 +17,32 @@
 using namespace std;
 using namespace pandemic;
 
-TEST_CASE("base case") {
-    Board board;
-    board[City::Atlanta] = 2;
-    cout << board << endl;
+static const int MAX_CITIES = 48;
+static const int MIN_CITIES = 0;
+static const int MAX_CUBES = 5;
+static const int MIN_CUBES = 1;
+static const int NUMBER_OF_CHECKS_LOOP = 500;
 
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
-    CHECK(3 + 6 == 9);
+static int get_random_city() {
+    return MIN_CITIES + (rand() % (MAX_CITIES - MIN_CITIES + 1));
+}
+
+static int get_random_cube() {
+    return MIN_CUBES + (rand() % (MAX_CUBES - MIN_CUBES + 1));
+}
+
+TEST_CASE("board is_clean method check") {
+    Board board;
+
+    int i = 0;
+    while(i < NUMBER_OF_CHECKS_LOOP) {
+        int random_city = get_random_city();
+        CHECK(board.is_clean() == true);
+        int random_cube = get_random_cube();
+        board[random_city] = random_cube;
+        CHECK(board.is_clean() == false);
+        board[random_city] = 0;
+        CHECK(board.is_clean() == true);
+        ++i;
+    }
 }
