2895cc4491ae106b5589a7000c2cae2b78ae1dd0
kfir <kggold4@gmail.com>
2021-05-13 12:04:13 +0300

    update player

--- sources/Player.cpp
+++ sources/Player.cpp
@@ -17,7 +17,7 @@ using namespace pandemic;
 
 namespace pandemic {
 
-    Player::Player(Board& board, const int city) {
+    Player::Player(Board& board, const City city) {
         this->board = board;
         this->current_city = city;
         this->number_of_cards = 0;
@@ -25,7 +25,7 @@ namespace pandemic {
     }
     Player::~Player() {}
 
-    Player& Player::drive(const int city) {
+    Player& Player::drive(const City city) {
         Set<City> close_cities = city_map.at(this->current_city);
         for(auto &i : close_cities) {
             if(i == city) {
@@ -35,17 +35,20 @@ namespace pandemic {
         }
         throw invalid_argument("given city is not close to current city");
     }
-    Player& Player::fly_direct(const int city) {
+    Player& Player::fly_direct(const City city) {
+        if(this->number_of_cards == 0) { throw exception("not enugth carrds"); }
         if(!valid_city(city)) { throw invalid_argument("invalid given city - do not exist"); }
         this->current_city = city;
+        this->number_of_cards--;
         return *this;
     }
-    Player& Player::fly_charter(const int city) {
+    Player& Player::fly_charter(const City city) {
         if(!valid_city(city)) { throw invalid_argument("invalid given city - do not exist"); }
         this->current_city = city;
+        this->number_of_cards--;
         return *this;
     }
-    Player& Player::fly_shuttle(const int city) {
+    Player& Player::fly_shuttle(const City city) {
         if(!valid_city(city)) { throw invalid_argument("invalid given city - do not exist"); }
         if(!has_station[this->current_city]) {
             throw invalid_argument("current city has no stations");
@@ -62,16 +65,18 @@ namespace pandemic {
         has_station[this->current_city] = true;
         return *this;
     }
-    Player& Player::discover_cure(const int color) {
+    Player& Player::discover_cure(const City color) {
         return *this;
     }
-    Player& Player::treat(const int city) {
+    Player& Player::treat(const City city) {
         return *this;
     }
-    Player& Player::take_card(const int city) {
+    Player& Player::take_card(const City city) {
+        this->cards.insert(City(city));
         return *this;
     }
-    Player& Player::remove_cards() {
+    Player& Player::remove_cards(const City city) {
+
         return *this;
     }
     string Player::role() const {
--- sources/Player.hpp
+++ sources/Player.hpp
@@ -10,6 +10,7 @@
 
 #pragma once
 
+#include <set>
 #include "Board.hpp"
 #include "Color.hpp"
 #include "City.hpp"
@@ -22,26 +23,27 @@ namespace pandemic {
             Board board;
             int current_city;
             unsigned int number_of_cards;
-            bool valid_city(const int city) const {
+            set<City> cards;
+            bool valid_city(const City city) const {
                 if(city < 0 || city >= MAX_CITIES) { return false; }
                 return true;
             }
         public:
-            Player(Board& board, const int city);
+            Player(Board& board, const City city);
             ~Player();
 
             // drive to given near city (if not near throw exception)
-            virtual Player& drive(const int city);
+            virtual Player& drive(const City city);
 
             // fly to another city
-            virtual Player& fly_direct(const int city);
-            virtual Player& fly_charter(const int city);
-            virtual Player& fly_shuttle(const int city);
+            virtual Player& fly_direct(const City city);
+            virtual Player& fly_charter(const City city);
+            virtual Player& fly_shuttle(const City city);
             virtual Player& build();
-            virtual Player& discover_cure(const int color);
-            virtual Player& treat(const int city);
-            virtual Player& take_card(const int city);
-            virtual Player& remove_cards();
+            virtual Player& discover_cure(const City color);
+            virtual Player& treat(const City city);
+            virtual Player& take_card(const City city);
+            virtual Player& remove_cards(const City city);
             string role() const;
     };
 }
