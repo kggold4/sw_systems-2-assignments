f21c662d3fcbd0b394832ebecc1dee2098139972
kfir <kggold4@gmail.com>
2021-05-19 13:08:19 +0300

    add main rule to makefile

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/**
+ *
+ * Assignment #4
+ * COURSE:   Systems Programming 2 Ariel University
+ * AUTHORS:  Kfir Goldfarb
+ * https://www.github.com/kggold4
+ * YEAR: 2021
+ *
+ * */
+
+#include <iostream>
+#include <string>
+
+#include "sources/Board.hpp"
+#include "sources/City.hpp"
+#include "sources/Color.hpp"
+#include "sources/Player.hpp"
+
+#include "sources/Researcher.hpp"
+#include "sources/Scientist.hpp"
+#include "sources/FieldDoctor.hpp"
+#include "sources/GeneSplicer.hpp"
+#include "sources/OperationsExpert.hpp"
+#include "sources/Dispatcher.hpp"
+#include "sources/Medic.hpp"
+#include "sources/Virologist.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "this is main" << endl;
+    return 0;
+}
